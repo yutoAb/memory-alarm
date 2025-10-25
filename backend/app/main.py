d9c23f1be737acffcbc779841a168ed5c4561159
yutoAb <yuto27abe@gmail.com>
2025-10-25 11:43:28 +0900

    アイテム取得エンドポイントを追加し、ユーザーアイテムの詳細を取得する機能を実装

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,9 +5,13 @@ app = FastAPI()
 async def root():
     return {"message" : "Hello World"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
 
 from enum import Enum
 class ModelName(str, Enum):
@@ -25,3 +29,17 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
+
+from typing import Union
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Union[str, None] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
