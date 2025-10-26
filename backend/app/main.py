99339ce5e31ef6a4f2a89e0388b196b94e145936
yutoAb <yuto27abe@gmail.com>
2025-10-26 21:56:53 +0900

    新規作成: FastAPIアプリケーションのルートエンドポイントとアイテム取得エンドポイントを追加

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,3 +8,11 @@ app = FastAPI()
 @app.on_event("startup")
 def on_startup():
     Base.metadata.create_all(bind=engine)  # 開発用: 無ければ作る
+
+@app.get("/")
+def read_root():
+    return {"Hello" : "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id" : item_id, "q": q}
